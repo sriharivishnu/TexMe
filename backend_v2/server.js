a116e5d90b61c41476757383a71a98c05ef99b54
Srihari Vishnu <srihari.vishnu@gmail.com>
2021-05-02 17:44:00 -0400

    Initial Commit

---
+++ backend_v2/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/latex", (req, res) => {
+  res.send(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
